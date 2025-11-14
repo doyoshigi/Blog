e3baf3ab384017bed4dff90296b52c8c7c820db7
doyoshigi <doyoshigi@gmail.com>
2025-11-14 19:28:53 +0900

    Quartz sync: Nov 14, 2025, 7:28 PM

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -53,7 +53,7 @@ export default ((userOpts?: Partial<Options>) => {
                       </a>
                     </h3>
                   </div>
-                  {page.dates && (
+                  {page.created && (
                     <p class="meta">
                       <Date date={getDate(cfg, page)!} locale={cfg.locale} />
                     </p>
