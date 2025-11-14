d491583a1b7e4e226b97acbb603f8c3218ccf46e
doyoshigi <doyoshigi@gmail.com>
2025-11-14 19:25:58 +0900

    Quartz sync: Nov 14, 2025, 7:25 PM

--- quartz/components/AllRecentNotes.tsx
+++ quartz/components/AllRecentNotes.tsx
@@ -1,14 +1,11 @@
 import { QuartzComponent, QuartzComponentProps } from "./types"
 import RecentNotes from "./RecentNotes"
-import { SimpleSlug } from "../util/path"
 
 const AllRecentNotes: QuartzComponent = (props: QuartzComponentProps) => {
   if (props.fileData.slug === "all-posts") {
     return RecentNotes({
-      title: "Recent Posts",
-      limit: 5,
+      limit: 999,
       showTags: true,
-      linkToMore: "all-posts" as SimpleSlug,
     })(props)
   }
   return null
